89e743fe906d38f415a553d6593a4a7ed6b40fa7
mrseidel <p0083466@2652TR206015F44.ad1.peelsb.com>
2014-11-27 09:56:52 -0500

    finalized LetterSolution for 2008

--- ecoo/2008/Solutions/LetterSolution2008.py
+++ ecoo/2008/Solutions/LetterSolution2008.py
@@ -1,5 +1,7 @@
-def calculation(a, b, x2, y2):
-    return 1/sqrt(pow((a - x), 2) + pow((b - y), 2))
+def dist(a, b):
+    if a[0] - b[0] == 0 and a[1] - b[1] == 0:
+        return 0
+    return 1.0/pow(pow((a[0] - b[0]), 2) + pow((a[1] - b[1]), 2),0.5)
 
 file = open("DATA31.txt", 'r')
 f = file.readlines()
@@ -7,7 +9,6 @@ f = file.readlines()
 arr = []
 letters = []
 for line in f:
-    print(line)
     if len(line) > 2:
         arr.append(line[:-1])
     if len(line) <=2:
@@ -15,6 +16,17 @@ for line in f:
             letters.append(line[:-1])
         else:
             letters.append(line)
-               
-print(arr)
-print(letters)
+
+for letter in letters:
+    calc = 0
+    positions = []
+    for x in range(len(arr)):
+        for y in range(len(arr[x])):
+            if arr[x][y] == letter:
+                positions.append((y+1, x+1))
+
+    for i in range(len(positions)):
+        for j in range(len(positions)):
+            calc += dist(positions[i], positions[j])
+    
+    print("the " + letter + "-solution = %.4f" % calc)    
